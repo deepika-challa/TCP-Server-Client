ccf6d1ecdcc3ad7ab35e6d1d0bc09958ecf8b339
deepika-challa <dech18@student.bth.se>
2020-12-09 22:06:58 +1100

    server and client start

--- TCP-Server-Client/src/Client.java
+++ TCP-Server-Client/src/Client.java
@@ -1,4 +1,18 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
+	
+	private String  Accp ="TEXT TCP 1.0";	
+	
+	public static void main(String[] args) throws IOException{
+		Integer port = 8080;
+		String add = "localhost";
+		Socket conn = new Socket(add,port);
+		
+
+	}
+	
 
 }
--- TCP-Server-Client/src/Server.java
+++ TCP-Server-Client/src/Server.java
@@ -1,10 +1,35 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		
+		Integer port = 8080;
+		ServerSocket serverSocket = new ServerSocket(port);
+		
+		while(true) {
+			Socket client = serverSocket.accept();
+			
+			DataInputStream in=new DataInputStream(client.getInputStream());  	  
+			DataOutputStream out=new DataOutputStream(client.getOutputStream());
+			
+			out.writeUTF("TEXT TCP 1.0\n");
+			out.writeUTF("\n");
+			
+			
+
+			
+		}
+		
+		
+
 	}
 	
 
 }
+	
